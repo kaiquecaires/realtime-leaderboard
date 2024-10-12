4420b6e1b6cb8d2961d29036a3a55b5bbf5cf615
kaique <caires.kaique@gmail.com>
2024-10-12 06:46:51 -0300

    feat: added game model

---
+++ cmd/models/game.go
@@ -0,0 +1,20 @@
+package models
+
+import "errors"
+
+type Game struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func (p *Game) Validate() error {
+	if len(p.Name) < 5 {
+		return errors.New("name must have at least 5 characters")
+	}
+
+	if len(p.Name) > 50 {
+		return errors.New("name must have at most 50 characters")
+	}
+
+	return nil
+}
