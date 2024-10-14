07072e026b4f6f47549ec1303ded89a949e09332
kaique <caires.kaique@gmail.com>
2024-10-14 06:16:45 -0300

    feat: added user score store

---
+++ cmd/databases/user_score.go
@@ -0,0 +1,25 @@
+package databases
+
+import (
+	"database/sql"
+	"kaiquecaires/real-time-leaderboard/cmd/models"
+)
+
+type UserScoreStore interface {
+	Insert(models.CreateUserScoreParams) error
+}
+
+type PostgresUserScoreStore struct {
+	conn *sql.DB
+}
+
+func NewPostgresUserScoreStore(conn *sql.DB) *PostgresUserScoreStore {
+	return &PostgresUserScoreStore{conn: conn}
+}
+
+func (s *PostgresUserScoreStore) Insert(params models.CreateUserScoreParams) error {
+	query := "INSERT INTO user_scores (user_id, game_id, score) VALUES ($1, $2, $3) RETURNING id"
+	var userScoreId int
+	err := s.conn.QueryRow(query, params.UserId, params.GameId, params.Score).Scan(&userScoreId)
+	return err
+}
