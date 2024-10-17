9b565995f41b9c83dd7d506dc7cf0a315c637b0c
kaique <caires.kaique@gmail.com>
2024-10-17 06:08:09 -0300

    feat: added get leaderboard query

--- cmd/db/user_score.go
+++ cmd/db/user_score.go
@@ -2,11 +2,13 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"kaiquecaires/real-time-leaderboard/cmd/models"
 )
 
 type UserScoreStore interface {
 	Insert(models.CreateUserScoreParams) error
+	GetLeaderboard(models.GetLeaderboardParams) ([]models.Leaderboard, error)
 }
 
 type PostgresUserScoreStore struct {
@@ -22,4 +24,41 @@ func (s *PostgresUserScoreStore) Insert(params models.CreateUserScoreParams) err
 	var userScoreId int
 	err := s.conn.QueryRow(query, params.UserId, params.GameId, params.Score).Scan(&userScoreId)
 	return err
+}
+
+func (s *PostgresUserScoreStore) GetLeaderboard(params models.GetLeaderboardParams) ([]models.Leaderboard, error) {
+	query := `
+  SELECT username, SUM(score) as total_score FROM user_scores us
+  INNER JOIN users u ON u.id = us.user_id
+  GROUP BY u.id ORDER BY total_score DESC
+  LIMIT $1 OFFSET $2;
+
+  `
+
+	if params.Limit == 0 {
+		params.Limit = 10
+	}
+
+	rows, err := s.conn.Query(query, params.Limit, params.Offset)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var leaderboard []models.Leaderboard
+
+	for rows.Next() {
+		var user models.Leaderboard
+		err := rows.Scan(&user.Username, &user.Score)
+		if err != nil {
+			return nil, err
+		}
+		leaderboard = append(leaderboard, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("rows iteration error: %w", err)
+	}
+
+	return leaderboard, nil
 }
