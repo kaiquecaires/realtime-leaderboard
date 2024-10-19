a62e2d41479a9728194178d631f05ff6f8202467
kaique <caires.kaique@gmail.com>
2024-10-19 17:09:54 -0300

    feat: added redis client

---
+++ cmd/db/redis.go
@@ -0,0 +1,12 @@
+package db
+
+import "github.com/redis/go-redis/v9"
+
+func GetRedisClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+		Protocol: 3,  // specify 2 for RESP 2 or 3 for RESP 3
+	})
+}
